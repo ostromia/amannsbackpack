1405eb882653b78437ac562759ff9cf2292665a0
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-11-06 19:34:03 +0000

    feat(csmtc): searchbar to query cosmetics by name

--- src/routes/cosmetics/+page.svelte
+++ src/routes/cosmetics/+page.svelte
@@ -2,18 +2,26 @@
     import cosmeticsJSON from "$lib/cosmetics.json";
     import gradeColors from "$lib/gradeColors.json";
     import "$lib/tf2build.css";
+
+    let searchQuery: string = "";
 </script>
 
 <svelte:head>
     <title>TF2 Cosmetics</title>
 </svelte:head>
 
+<div class="filters-wrapper">
+    <input bind:value={searchQuery} type="text" id="searchbar" placeholder="Search for items..">
+</div>
+
 <section id="table">
     {#each cosmeticsJSON as item}
+        { #if searchQuery.trim() == "" || item.name.toLowerCase().includes(searchQuery.trim()) }
             <div class="item-wrapper" id="{item.name}">
                 <img alt="{item.name}" class="item-image" src="{item.src}">
                 <div class="underline" style="background-color:{gradeColors[item.grade]}"></div>
             </div>
+        {/if}
     {/each}
 </section>
 
@@ -23,6 +31,24 @@
 
     :global(html) {
         background-color: $background;
+    }
+    :global(body) {
+        margin: 0;
+    }
+
+    .filters-wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+        padding: 1rem 2rem;
+        box-sizing: border-box;
+    }
+
+    #searchbar {
+        font-size: 1rem;
+        font-family: "tf2build";
+        color: lightgray;
+        background-color: $foreground;
     }
 
 	#table {
