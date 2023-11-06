053783cae30b9211c2d774c3423d08b4f0e250b9
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2023-11-06 19:31:44 +0000

    feat(csmtc): display cosmetics in flexbox
    • display cosmetics in tabular flex box
    • underlines to signify cosmetic grade
    • simple styling to match inventory UI

---
+++ src/routes/cosmetics/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import cosmeticsJSON from "$lib/cosmetics.json";
+    import gradeColors from "$lib/gradeColors.json";
+    import "$lib/tf2build.css";
+</script>
+
+<svelte:head>
+    <title>TF2 Cosmetics</title>
+</svelte:head>
+
+<section id="table">
+    {#each cosmeticsJSON as item}
+            <div class="item-wrapper" id="{item.name}">
+                <img alt="{item.name}" class="item-image" src="{item.src}">
+                <div class="underline" style="background-color:{gradeColors[item.grade]}"></div>
+            </div>
+    {/each}
+</section>
+
+<style lang="scss">
+    $background: #1A1411;
+    $foreground: #342E29;
+
+    :global(html) {
+        background-color: $background;
+    }
+
+	#table {
+		display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+		gap: 1rem;
+    }
+
+    .item-wrapper {
+        background-color: $foreground;
+    }
+    .item-image {
+        height: 100px;
+        display: block;
+        margin: auto;
+    }
+    .underline {
+        height: 3px;
+    }
+</style>
