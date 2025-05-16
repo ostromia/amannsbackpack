7f5e31651651bf91a0f431e566448cd289cb266d
ostromia <66202981+ostromia@users.noreply.github.com>
2025-05-16 06:09:16 +0100

    Fri 16 May 2025 06:09:16 BST

---
+++ _app/immutable/nodes/0.CYi39MnT.js
@@ -0,0 +1,1 @@
+import{c as n,a}from"../chunks/DoyU_rqs.js";import{k as p}from"../chunks/DXFQd4I8.js";import{s}from"../chunks/DEFVFPco.js";const i=!0,f=Object.freeze(Object.defineProperty({__proto__:null,prerender:i},Symbol.toStringTag,{value:"Module"}));function u(o,r){var e=n(),t=p(e);s(t,()=>r.children),a(o,e)}export{u as component,f as universal};
